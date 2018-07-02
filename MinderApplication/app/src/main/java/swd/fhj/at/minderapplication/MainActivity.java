129d8aa55f05d82c97c753b9c220047c62c0849b
Robert Ionescu <ionescurobert@users.noreply.github.com>
2018-07-02 22:21:27 +0200

    bottom bar added with new app
    Created new application with bottom nav bar
    - still a bit buggy

---
+++ MinderApplication/app/src/main/java/swd/fhj/at/minderapplication/MainActivity.java
@@ -0,0 +1,69 @@
+package swd.fhj.at.minderapplication;
+
+import android.support.v4.app.Fragment;
+import android.app.FragmentTransaction;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.FrameLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    private BottomNavigationView mMainNav;
+    private FrameLayout mMainFrame;
+
+    private HomeFragment homeFragment;
+    private NotificationFragment notificationFragment;
+    private AccountFragment accountFragment;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        mMainFrame = (FrameLayout) findViewById(R.id.main_frame);
+        mMainNav = (BottomNavigationView) findViewById(R.id.main_nav);
+
+        homeFragment = new HomeFragment();
+        notificationFragment = new NotificationFragment();
+        accountFragment = new AccountFragment();
+
+        setFragment(homeFragment);
+
+        mMainNav.setOnNavigationItemReselectedListener(new BottomNavigationView.OnNavigationItemReselectedListener() {
+            @Override
+            public void onNavigationItemReselected(@NonNull MenuItem item) {
+
+                switch (item.getItemId()) {
+
+                    case R.id.nav_home:
+                        mMainNav.setItemBackgroundResource(R.color.colorPrimary);
+                        setFragment(homeFragment);
+
+                    case R.id.nav_notif:
+                        mMainNav.setItemBackgroundResource(R.color.colorAccent);
+                        setFragment(notificationFragment);
+
+                    case R.id.nav_account:
+                        mMainNav.setItemBackgroundResource(R.color.colorPrimaryDark);
+                        setFragment(accountFragment);
+                    default:
+
+
+                }
+            }
+        });
+    }
+
+    private void setFragment(Fragment fragment) {
+
+        android.support.v4.app.FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.main_frame, fragment);
+        fragmentTransaction.commit();
+
+
+    }
+}
