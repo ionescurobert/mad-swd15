bc76c337f025bec4d4b3ab772dde4bca316e085d
Robert Ionescu <ionescurobert@users.noreply.github.com>
2018-07-04 20:15:45 +0200

    Rechanged API

---
+++ Minder/app/src/main/java/swd/fhj/at/minder/db/TaskContract.java
@@ -0,0 +1,14 @@
+package swd.fhj.at.minder.db;
+
+import android.provider.BaseColumns;
+
+public class TaskContract {
+    public static final String DB_NAME = "swd.fhj.at.minder";
+    public static final int DB_VERSION = 1;
+
+    public class TaskEntry implements BaseColumns {
+        public static final String TABLE = "tasks";
+
+        public static final String COL_TASK_TITLE = "title";
+    }
+}
